aa584a58b9d66442893809c4450d3d1ad7fbf721
heonwoochoo <cngjsdn9@gmail.com>
2023-03-29 11:23:46 +0900

    backup

---
+++ baekjoon/week3/Header.h
@@ -0,0 +1,13 @@
+#pragma once
+#include <iostream>
+
+using namespace std;
+
+void solution()
+{
+	ios_base::sync_with_stdio(false);
+	cin.tie(NULL);
+	cout.tie(NULL);
+
+	cout << "Hello wordl" << endl;
+}
---
+++ baekjoon/week3/main.cpp
@@ -0,0 +1,7 @@
+#include "Header.h"
+
+int main()
+{
+	solution();
+	return 0;
+}
